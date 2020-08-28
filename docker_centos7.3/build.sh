d69815821d7ca0d241ecab05c5fc7356e74b39e9
heavenbeing <heavenbeing@outlook.com>
2020-08-28 11:30:14 +0800

    修改镜像名，加上韭菜

--- docker_centos7.3/build.sh
+++ docker_centos7.3/build.sh
@@ -2,7 +2,7 @@
 
 dir=$(cd $(dirname $0); pwd)
 project_dir=centos
-project_name=pythonstock
+project_name=pythonstockleeks
 echo ${dir}
 
 #version=$1
@@ -13,11 +13,11 @@ echo ${dir}
 
 NOW_MONTH=$(date "+%Y-%m")
 
-DOCKER_TAG=pythonstock/pythonstock:base-${NOW_MONTH}
+DOCKER_TAG=pythonstockleeks/pythonstockleeks:base-${NOW_MONTH}
 
 echo " docker build -f Dockerfile -t ${DOCKER_TAG} ."
 docker build -f Dockerfile -t ${DOCKER_TAG} .
-docker save -o ${dir}/../images/pythonstock.tar ${DOCKER_TAG}
+docker save -o ${dir}/../images/pythonstockleeks.tar ${DOCKER_TAG}
 echo "#################################################################"
 echo " docker push ${DOCKER_TAG} "
 
